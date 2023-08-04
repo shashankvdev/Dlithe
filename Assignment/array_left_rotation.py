6e4224860db8dad6b38b9138c79cb992b19de632
shashankvdev <135131075+shashankvdev@users.noreply.github.com>
2023-08-04 12:43:48 +0530

    Add files via upload

---
+++ Assignment/array_left_rotation.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+    a=[]
+    rem=d%len(arr)
+    a.append(arr[rem])
+    for i in range(len(arr)-1):
+        rem+=1
+        if(rem>len(arr)-1):
+            rem=0
+        a.append(arr[rem])
+        
+        
+        
+        
+    
+    return(a)       
+         
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
